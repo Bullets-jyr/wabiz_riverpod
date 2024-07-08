5e8dfd47bebeb95d98af4b405715a275cc97465c
Bullets-jyr <jyr@bullets.co.kr>
2024-07-08 10:13:11 +0900

    Provider 읽기 실습

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wabiz_riverpod/provider/counter_provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  // for dart
+  // final container = ProviderContainer();
+  // final counterClass = container.read(counterProvider);
+  // print(counterClass.counterValue);
+  // counterClass.increment();
+  // print(counterClass.counterValue);
+
+  // final counter2Class = container.read(counter2Provider);
+  // print(counter2Class.counterValue);
+  // counter2Class.increment();
+  // print(counter2Class.counterValue);
+
+  runApp(
+    ProviderScope(
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -9,10 +27,23 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
+        ),
+        floatingActionButton: Consumer(
+          builder: (context, ref, child) {
+            final counter = ref.read(counterProvider);
+            return FloatingActionButton(
+              onPressed: () {
+                counter.increment();
+              },
+              child: Icon(
+                Icons.add,
+              ),
+            );
+          }
         ),
       ),
     );
