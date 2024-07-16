e83d04f4d5f46f0545b50746fae78a83793579b5
Bullets-jyr <jyr@bullets.co.kr>
2024-07-16 13:28:09 +0900

    StateProvider 실습

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wabiz_riverpod/provider/counter_provider.dart';
+import 'package:wabiz_riverpod/state_provider/my_state_provider.dart';
 
 import 'provider/counter_cfw.dart';
 import 'provider/counter_consumer_widget.dart';
@@ -32,34 +33,23 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          // child: CounterWidget(),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CounterWidget(),
-              CounterStatefulWidget(),
-              Consumer(
-                builder: (context, ref, child) {
-                  final counter = ref.read(counterProvider);
-                  return ElevatedButton(
-                    onPressed: () {
-                      counter.decrement();
-                    },
-                    child: Text(
-                      '감소',
-                    ),
-                  );
-                },
+        body: Consumer(
+          builder: (context, ref, child) {
+            final counter = ref.watch(counterStateProvider);
+            return Center(
+              child: Text(
+                '$counter',
               ),
-            ],
-          ),
+            );
+          },
         ),
         floatingActionButton: Consumer(builder: (context, ref, child) {
           final counter = ref.read(counterProvider);
           return FloatingActionButton(
             onPressed: () {
-              counter.increment();
+              ref
+                  .read(counterStateProvider.notifier)
+                  .update((state) => state += 1);
             },
             child: Icon(
               Icons.add,
